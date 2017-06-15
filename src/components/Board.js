710082a9372391ced24bd275460e8606b4d27fd7
Ivan Macek <imacek@gmail.com>
2017-06-15 07:35:57 -0700

    now selecting blocks and heeping clicked history

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,48 +5,40 @@ class Board extends React.Component {
     super(props)
 
     this.state = {
-      visited: false,
-      word: [],
-      active: null,
+      selectedBlocks: []
     }
   }
 
-  onHover() {
-    this.setState({ visited: true })
+  blockClick(row, column) {
+    const { configuration } = this.props
 
-    console.log('hej')
+    this.setState(oldState => {
+      return {
+        selectedBlocks: oldState.selectedBlocks.concat({
+          row,
+          column,
+          value: configuration[row][column]
+        })
+      }
+    })
   }
 
-
-  changeColor(index) {
-    if (this.state.active === index) {
-      return "purple";
-    }
-    return "#D8D8D8";
+  isBlockSelected(row, column) {
+    return this.state.selectedBlocks.some(selectedBlock => selectedBlock.row === row && selectedBlock.column === column)
   }
-  
-  clickOnBtn(index, row) {
-    const letter = row[index]
-    this.state.word.push(letter)
-    let string = this.state.word.join("")
-    //check which row is clicked
-    if (this.state.active === index) {
-      this.setState({active : null})
-    } else {
-      this.setState({active : index})
-    }
 
-    console.log(string)
+  blockClassName(row, column) {
+    return 'dice' + (this.isBlockSelected(row, column) ? ' selected' : '')
   }
 
   render() {
     const { configuration } = this.props
     return (
       <div>
-        {configuration.map((row, index) =>
-          <div key={index} className='board-row'>
-            {row.split('').map((character, index) =>
-              <button key={index} className='dice' style={{ backgroundColor: this.changeColor(index)}} onMouseOver={() => this.onHover()} onClick={() => this.clickOnBtn(index,row)} >{character}</button>)
+        {configuration.map((row, rowIndex) =>
+          <div key={rowIndex} className='board-row'>
+            {row.split('').map((character, columnIndex) =>
+              <button key={columnIndex} className={this.blockClassName(rowIndex, columnIndex)} onClick={() => this.blockClick(rowIndex, columnIndex)}>{character}</button>)
             }
           </div>)
         }
